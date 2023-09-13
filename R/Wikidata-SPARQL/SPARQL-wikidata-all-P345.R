38feb6b921344d4b3cac25fa884c2d830ca7c03a
Jacob Greer <jacobmgreer@users.noreply.github.com>
2023-09-13 00:06:45 -0400

    updates

--- R/Wikidata-SPARQL/SPARQL-wikidata-all-P345.R
+++ R/Wikidata-SPARQL/SPARQL-wikidata-all-P345.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(jsonlite)
 library(magrittr)
 
-options(readr.show_col_types = FALSE)
+#options(readr.show_col_types = FALSE)
 
 GetIMDbCount <-
   as.numeric(query_wikidata("SELECT (COUNT(*) as ?cnt) WHERE {?s wdt:P345 ?o}"))
@@ -12,19 +12,23 @@ IMDbPasses <- ceiling(GetIMDbCount / 50000)
 WikiData.Films <- NULL
 
 for (i in 1:IMDbPasses) {
-  WikiData.Films <- bind_rows(WikiData.Films,
-                              query_wikidata(paste("
-        SELECT ?work ?imdb
-        WHERE {
-          ?work wdt:P345 ?imdb.
-        }
-        LIMIT 50000 OFFSET ",
-        format((i-1) * 50000, scientific = F))
-  ))
-  #Sys.sleep(5)
-}
+  WikiData.Films <-
+    bind_rows(WikiData.Films,
+              query_wikidata(paste("
+                SELECT ?work ?imdb
+                WHERE {
+                  ?work wdt:P345 ?imdb.
+                }
+                LIMIT 50000 OFFSET ",
+                format((i-1) * 50000, scientific = F))
+              ))
+              #Sys.sleep(5)
+            }
 
-WikiData.Films <- WikiData.Films %>% distinct()
+WikiData.Films <-
+  WikiData.Films %>%
+  distinct() %T>%
+  write.csv(., file = "output/WikiData-ComprehensiveFilms.csv")
 
 WikiData.IMDBDistinct <- WikiData.Films %>% distinct(imdb, .keep_all = TRUE)
 
