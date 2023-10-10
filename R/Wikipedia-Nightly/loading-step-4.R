8086343a0d8b51ab94e229cf8142cb090c1e183c
Jacob Greer <jacobmgreer@users.noreply.github.com>
2023-10-10 05:34:16 -0400

    updates

--- R/Wikipedia-Nightly/loading-step-4.R
+++ R/Wikipedia-Nightly/loading-step-4.R
@@ -6,12 +6,16 @@ library(qdapRegex)
 find.imdb.tt <-
   films %>%
   filter(grepl("imdb.com/title",external_link)) %>%
+  filter(grepl("classification.ores.articletopic/Culture.Media.Films", weighted_tags)) %>%
   mutate(
     FilmID = str_extract_all(external_link, "(?<=title/tt)\\d+")) %>%
   unnest(FilmID) %>%
   mutate(
     FilmID = paste0("tt", FilmID)) %>%
-  select(-c(name, weighted_tags, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %T>%
+  select(-c(name, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %>%
+  left_join(data %>% select(tconst, primaryTitle), by=c("FilmID" = "tconst")) %>%
+  mutate(status = ifelse(title == primaryTitle, "match", "not exact match")) %>%
+  distinct() %T>%
   write.csv(., "output/wiki-imdb-films.csv", row.names = FALSE)
 
 find.imdb.co <-
@@ -22,18 +26,23 @@ find.imdb.co <-
   unnest(CompanyID) %>%
   mutate(
     CompanyID = paste0("co", CompanyID)) %>%
-  select(-c(name, weighted_tags, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %T>%
+  select(-c(name, weighted_tags, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %>%
+  distinct() %T>%
   write.csv(., "output/wiki-imdb-companies.csv", row.names = FALSE)
 
 find.imdb.nm <-
   films %>%
   filter(grepl("imdb.com/name",external_link)) %>%
+  filter(grepl("classification.ores.articletopic/Culture.Biography.Biography", weighted_tags)) %>%
   mutate(
     PersonID = str_extract_all(external_link, "(?<=name/nm)\\d+")) %>%
   unnest(PersonID) %>%
   mutate(
     PersonID = paste0("nm", PersonID)) %>%
-  select(-c(name, weighted_tags, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %T>%
+  select(-c(name, source_text, text, opening_text, external_link, text_bytes, popularity_score)) %>%
+  left_join(name_basics_tsv %>% select(nconst, primaryName), by=c("PersonID" = "nconst")) %>%
+  mutate(status = ifelse(title == primaryName, "match", "not exact match")) %>%
+  distinct() %T>%
   write.csv(., "output/wiki-imdb-people.csv", row.names = FALSE)
 
 find.wiki.prod <-
