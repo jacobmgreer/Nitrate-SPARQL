ea18053b37796662217da4253186708c6085b94a
Jacob Greer <jacobmgreer@users.noreply.github.com>
2023-10-09 23:07:01 -0400

    updates

--- R/SPARQL/Query QID Production Companies.R
+++ R/SPARQL/Query QID Production Companies.R
@@ -10,42 +10,25 @@ QueryData <-
   ) %>%
   bind_rows(.,
     query_wikidata(sprintf(read_file('R/SPARQL/queries/P31.prod.P17.sparql'), "Q10689397"))) %>%
-  rename(QID=item) %>%
+  rename(QID = item) %>%
   mutate(QID = basename(QID)) %>%
   distinct() %T>%
   write.csv(., file = "output/SPARQL/wikidata-companies-country.csv", row.names = FALSE)
 
 QueryData <-
-  QueryData %>%
-  rename(QID=work) %>%
-  mutate(QID = basename(QID)) %>%
-  distinct() %T>%
-  write.csv(., file = "output/SPARQL/wikidata-films-eidr.csv", row.names = FALSE)
-
-
-QueryData <-
   query_wikidata(
     read_file('R/SPARQL/queries/P31.prod.P345.sparql')
   ) %>%
-  rename(QID=item) %>%
+  rename(QID = item) %>%
   mutate(QID = basename(QID)) %>%
   distinct() %T>%
   write.csv(., file = "output/SPARQL/wikidata-companies-imdb.csv", row.names = FALSE)
 
 QueryData <-
   query_wikidata(
-    read_file('R/SPARQL/queries/P31.prod.P17.sparql')
-  ) %>%
-  rename(QID=item) %>%
-  mutate(QID = basename(QID)) %>%
-  distinct() %T>%
-  write.csv(., file = "output/SPARQL/wikidata-companies-country.csv", row.names = FALSE)
-
-QueryData <-
-  query_wikidata(
     read_file('R/SPARQL/queries/P31.prod.sparql')
   ) %>%
-  rename(QID=item) %>%
+  rename(QID = item) %>%
   mutate(QID = basename(QID)) %>%
   distinct() %T>%
   write.csv(., file = "output/SPARQL/wikidata-companies-QID.csv", row.names = FALSE)
